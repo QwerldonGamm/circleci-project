799c3a4aaf70218eed5360e18e0e25f2639deaae
Yunsik <qkr7260@autuni.ac.nz>
2023-09-29 10:40:52 +1300

    shitean

--- main.py
+++ main.py
@@ -1,9 +1,10 @@
 def to_upper(name):
-return name.upper()
+    return name.upper()
+
 def say_hello(name):
-print(f'Hello, {name}')
-if __name__ == '__main__':
-name = 'Tony'
-say_hello(name)
-up = to_upper(name)
-print(up)
+    print(f'Hello, {name}')
+    if __name__ == '__main__':
+        name = 'Tony'
+        say_hello(name)
+        up = to_upper(name)
+        print(up)
